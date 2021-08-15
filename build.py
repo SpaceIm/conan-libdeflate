321e42df711121a61369b5faf1765e0c7fcbc7bb
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-08-16 00:08:49 +0200

    fix CI

--- build.py
+++ build.py
@@ -1,7 +1,17 @@
-#!/usr/bin/env python
-
-from bincrafters import build_template_default
+from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = build_template_default.get_builder(build_policy="missing")
+    recipe_name = "libdeflate"
+    recipe_version = "1.8"
+    username = "SpaceIm"
+    channel = "testing"
+    reference = "{}/{}@{}/{}".format(recipe_name, recipe_version, username, channel)
+
+    shared_option_name = "{}:shared".format(recipe_name)
+    pure_c = True
+
+    builder = ConanMultiPackager(username=username, channel=channel,
+                                 build_policy="missing", skip_check_credentials=True)
+    builder.add_common_builds(shared_option_name=shared_option_name, pure_c=pure_c, dll_with_static_runtime=True,
+                              reference=reference, build_all_options_values=None)
     builder.run()
